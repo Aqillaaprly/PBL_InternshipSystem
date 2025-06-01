1d59d11209f1ae7204503750ff02ffcf3910b3ea
Fahreiza <114044591+Fahreiza@users.noreply.github.com>
2025-06-01 14:39:00 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\UserController; // Untuk manajemen user data
 use App\Http\Controllers\Admin\AdminDashboardController;
-use App\Http\Controllers\Admin\CompanyController as AdminCompanyController; // Alias untuk kejelasan
+use App\Http\Controllers\Admin\CompanyController as AdminCompanyController;
 use App\Http\Controllers\Admin\LowonganController as AdminLowonganController;
 use App\Http\Controllers\Admin\PendaftarController as AdminPendaftarController;
 use App\Http\Controllers\Admin\MahasiswaController as AdminMahasiswaController;
@@ -25,9 +26,7 @@ Route::middleware(['auth', 'authorize:admin'])->prefix('admin')->name('admin.')-
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
 
     // Manajemen User Data (oleh Admin)
-    // Sebaiknya UserController ini ada di namespace Admin jika khusus untuk admin
-    // Jika UserController digunakan general, pastikan ada proteksi role di dalamnya atau pisahkan
-    Route::get('/users', [UserController::class, 'view'])->name('users.index'); // Mengganti 'userdata' menjadi 'users' untuk konsistensi
+    Route::get('/users', [UserController::class, 'view'])->name('users.index');
     Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
     Route::post('/users', [UserController::class, 'store'])->name('users.store');
     Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
@@ -35,49 +34,42 @@ Route::middleware(['auth', 'authorize:admin'])->prefix('admin')->name('admin.')-
     Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
     // Manajemen Perusahaan oleh Admin
-    Route::resource('perusahaan', AdminCompanyController::class)->except(['destroy']); // Menggunakan resource controller
-    Route::delete('perusahaan/{perusahaan}', [AdminCompanyController::class, 'destroy'])->name('perusahaan.destroy'); // Route delete terpisah jika ada konfirmasi khusus
+    // Jika AdminCompanyController adalah resource controller standar, Anda bisa menyederhanakannya:
+    Route::resource('perusahaan', AdminCompanyController::class);
+    // Jika Anda tetap memerlukan route 'destroy' terpisah karena alasan tertentu,
+    // pastikan 'except' di resource controller sudah benar atau hapus 'except' jika 'destroy' ditangani oleh resource.
+    // Untuk saat ini, saya asumsikan resource controller sudah menangani destroy.
 
     // Manajemen Lowongan oleh Admin
-    Route::resource('lowongan', AdminLowonganController::class)->except(['destroy']);
-    Route::delete('lowongan/{lowongan}', [AdminLowonganController::class, 'destroy'])->name('lowongan.destroy');
-
+    Route::resource('lowongan', AdminLowonganController::class);
 
     // Manajemen Pendaftar oleh Admin
-    Route::resource('pendaftar', AdminPendaftarController::class)->except(['destroy']);
-     Route::delete('pendaftar/{pendaftar}', [AdminPendaftarController::class, 'destroy'])->name('pendaftar.destroy');
-
+    Route::resource('pendaftar', AdminPendaftarController::class);
 
     // Data Mahasiswa (dikelola Admin)
     Route::get('/data-mahasiswa', [AdminMahasiswaController::class, 'index'])->name('datamahasiswa');
     Route::get('/data-mahasiswa/create', [AdminMahasiswaController::class, 'create'])->name('mahasiswa.create');
     Route::post('/data-mahasiswa', [AdminMahasiswaController::class, 'store'])->name('mahasiswa.store');
-    Route::get('/data-mahasiswa/{mahasiswa}/show', [AdminMahasiswaController::class, 'show'])->name('mahasiswa.show'); // show (User $mahasiswa)
-    Route::get('/data-mahasiswa/{mahasiswa}/edit', [AdminMahasiswaController::class, 'edit'])->name('mahasiswa.edit'); // edit (User $mahasiswa)
-    Route::put('/data-mahasiswa/{mahasiswa}', [AdminMahasiswaController::class, 'update'])->name('mahasiswa.update'); // update (User $mahasiswa)
-    Route::delete('/data-mahasiswa/{mahasiswa}', [AdminMahasiswaController::class, 'destroy'])->name('mahasiswa.destroy'); // destroy (User $mahasiswa)
-
-
-    Route::get('/data-pembimbing', [AdminPembimbingController::class, 'index'])->name('data_pembimbing');
-    Route::get('/laporan', [AdminLaporanController::class, 'index'])->name('laporan');
-    Route::get('/profile', [AdminProfileController::class, 'show'])->name('profile');
-    Route::get('/profile/edit', [AdminProfileController::class, 'edit'])->name('profile.edit');
-    Route::put('/profile', [AdminProfileController::class, 'update'])->name('profile.update');
+    // Pastikan {mahasiswa} di sini adalah instance dari User, bukan Mahasiswa model,
+    // jika AdminMahasiswaController menggunakan route model binding untuk User.
+    Route::get('/data-mahasiswa/{mahasiswa}/show', [AdminMahasiswaController::class, 'show'])->name('mahasiswa.show');
+    Route::get('/data-mahasiswa/{mahasiswa}/edit', [AdminMahasiswaController::class, 'edit'])->name('mahasiswa.edit');
+    Route::put('/data-mahasiswa/{mahasiswa}', [AdminMahasiswaController::class, 'update'])->name('mahasiswa.update');
+    Route::delete('/data-mahasiswa/{mahasiswa}', [AdminMahasiswaController::class, 'destroy'])->name('mahasiswa.destroy');
 
     // Data Pembimbing (dikelola Admin)
     Route::get('/data-pembimbing', [AdminPembimbingController::class, 'index'])->name('data_pembimbing');
-    // CRUD untuk pembimbing oleh admin bisa ditambahkan di sini
+    // CRUD untuk pembimbing oleh admin bisa ditambahkan di sini jika diperlukan
 
     // Laporan (dikelola Admin)
     Route::get('/laporan', [AdminLaporanController::class, 'index'])->name('laporan');
 
     // Profil Admin
     Route::get('/profile', [AdminProfileController::class, 'show'])->name('profile');
-    Route::get('/profile/edit', [AdminProfileController::class, 'edit'])->name('profile.edit'); // Tambahkan route untuk edit profile
-    Route::put('/profile', [AdminProfileController::class, 'update'])->name('profile.update'); // Tambahkan route untuk update profile
+    Route::get('/profile/edit', [AdminProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/profile', [AdminProfileController::class, 'update'])->name('profile.update');
 
-    // Contoh route jika ada halaman pengaturan khusus admin
-    // Route::get('/pengaturan', [AdminPengaturanController::class, 'index'])->name('pengaturan');
+    // Rute yang duplikat telah dihapus. Definisi di atas sudah mencakupnya.
 });
 
 // DOSEN GROUP
@@ -91,10 +83,7 @@ Route::middleware(['auth', 'authorize:dosen'])->prefix('dosen')->name('dosen.')-
 // MAHASISWA GROUP
 Route::middleware(['auth', 'authorize:mahasiswa'])->prefix('mahasiswa')->name('mahasiswa.')->group(function () {
     Route::get('/dashboard', function () {
-        // Contoh jika mahasiswa dashboard butuh data spesifik
-        // $user = Auth::user();
-        // $pendaftaranSaya = Pendaftar::where('user_id', $user->id)->with('lowongan.company')->get();
-        // return view('mahasiswa.dashboard', compact('user', 'pendaftaranSaya'));
+        // Path view diperbaiki dari 'admin.mahasiswa.dashboard' menjadi 'mahasiswa.dashboard'
         return view('mahasiswa.dashboard'); // Pastikan view ini ada: resources/views/mahasiswa/dashboard.blade.php
     })->name('dashboard');
 
@@ -109,10 +98,6 @@ Route::middleware(['auth', 'authorize:mahasiswa'])->prefix('mahasiswa')->name('m
 // PERUSAHAAN GROUP
 Route::middleware(['auth', 'authorize:perusahaan'])->prefix('perusahaan')->name('perusahaan.')->group(function () {
     Route::get('/dashboard', function () {
-        // $company = Auth::user()->company; // Asumsi ada relasi company di model User perusahaan
-        // $lowongans = Lowongan::where('company_id', $company->id)->get();
-        // $pendaftars = Pendaftar::whereIn('lowongan_id', $lowongans->pluck('id'))->count();
-        // return view('company.dashboard', compact('company', 'lowongans', 'pendaftars'));
         return view('company.dashboard'); // Pastikan view ini ada: resources/views/company/dashboard.blade.php
     })->name('dashboard');
 
@@ -120,6 +105,8 @@ Route::middleware(['auth', 'authorize:perusahaan'])->prefix('perusahaan')->name(
     Route::get('/pendaftar', function () { return view('company.pendaftar'); })->name('pendaftar');
     Route::get('/tambah-lowongan', function () { return view('company.tambah_lowongan'); })->name('tambah_lowongan');
     // Proses tambah lowongan dan pendaftar sebaiknya menggunakan Controller
-    // Route::post('/lowongan', [CompanyLowonganController::class, 'store'])->name('lowongan.store');
-    // Route::post('/pendaftar/update-status', [CompanyPendaftarController::class, 'updateStatus'])->name('pendaftar.update_status');
+    // Contoh:
+    // Route::post('/lowongan', [App\Http\Controllers\Company\LowonganController::class, 'store'])->name('lowongan.store');
+    // Route::post('/pendaftar/update-status', [App\Http\Controllers\Company\PendaftarController::class, 'updateStatus'])->name('pendaftar.update_status');
 });
+
